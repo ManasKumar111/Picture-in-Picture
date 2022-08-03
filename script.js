f1140e2a5082172b8f7866e2232d393a3a87b305
Manas Kumar <manasansha@gmail.com>
2022-08-04 00:28:49 +0530

    Picture-in-Picture Website

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Hello world');
+const videoElement=document.getElementById('video');
+const button=document.getElementById('button');
+
+//Prompt the user to select a mediastream ,passto video element, then play
+async function selectMediaStream(){
+    try{
+        const mediaStream=await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject=mediaStream;
+        videoElement.onloadedmetadata=() =>{
+            videoElement.play()
+        }
+    }catch(error){
+        //we will catch errors here
+        console.log("whoops error here:",error)
+    }
+}
+button.addEventListener('click',async ()=>{
+    button.disabled=true;
+    // Start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset the button
+    button.disabled=false;
+});
+//onload
+selectMediaStream();
